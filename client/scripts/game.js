d8b0861cff617d7b14ec2005768e1e05d0f22a4d
JTKlemmensen <jonastklemmensen@gmail.com>
2019-03-08 16:43:42 +0100

    Refactor and removed unused code

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -6,7 +6,7 @@ var towerDefense = {};
 $(function () 
 {
     loadImages();
-    console.log(';:D');
+
     var fps = 60;
     var updateRate = 1000/fps;
     window.setInterval(towerDefense.events.onDraw, updateRate);
@@ -24,7 +24,7 @@ $(function ()
 const times = [];
 let fps;
 
-function refreshLoop() {
+function updateFps() {
   window.requestAnimationFrame(() => {
     const now = performance.now();
     while (times.length > 0 && times[0] <= now - 1000) {
@@ -33,17 +33,16 @@ function refreshLoop() {
     times.push(now);
     fps = times.length;
     $("#fps").text(fps);
-    //refreshLoop();
   });
 }
 var imgs = [];
 
 function loadImages()
 {
-    loadImage('test');
-    loadImage('test1');
-    loadImage('test2');
-    loadImage('test3');
+    var imgNames = ['test','test1','test2','test3'];
+    imgNames.forEach(function(el){
+        loadImage(el);
+    });
 }
 
 function loadImage(name)
@@ -87,36 +86,14 @@ var x = 0;
 var y= 0;
 function createSprite()
 {
-    refreshLoop();
     var img = {};
     var newImg = new Image;
     newImg.onload = function () {
-            var canvas = $("canvas")[0];
-    var context = canvas.getContext("2d");
-    context.fillStyle = "#FF0000";
-    context.fillRect(0,0,300,300);
-    
-    for(var i=0;i<entities.length;i++)
-    {
-        var ent = entities[i];
-    //context.drawImage(ent.sprite,ent.x,ent.y);
-    }
-    drawRotatedImg(imgs['test1'],50,50,x++,context);
     }
     newImg.src = '/images/test';
     
     img.Image = newImg;
     return img;
-}
-
-function drawRotatedImg(img, x, y, angle, context)
-{
-   context.save();
-   context.translate(x+img.width/2,y+img.height/2);
-   context.rotate(angle*Math.PI/180);
-   context.translate(-x-img.width/2,-y-img.height/2);
-   context.drawImage(img,x,y);
-   context.restore();
 }
 
 ///////////////////////////////////////////////////////////
@@ -169,22 +146,25 @@ towerDefense.events.onDraw = function()
     // Draw code
     var canvas = $("canvas")[0];
     var context = canvas.getContext("2d");
-    context.fillStyle = "#FF0000";
-    context.fillRect(0,0,canvas.width,canvas.height);
  
     for(var grassX =0;grassX<canvas.width/64;grassX++)
-    {
         for(var grassY =0;grassY<canvas.height/64;grassY++)
-        context.drawImage(imgs['test3'],grassX*64,grassY*64);    
-    }
-    /*   
-    for(var p=0;p<1000;p++)
-    for(var i=0;i<entities.length;i++)
-    {
-        var ent = entities[i];
-    context.drawImage(ent.sprite,ent.x,ent.y);
-    }
-*/
+            context.drawImage(imgs['test3'],grassX*64,grassY*64);    
+    
     drawRotatedImg(imgs['test1'],50,50,x++,context);
-    refreshLoop();
+    updateFps();
+}
+
+///////////////////////////////////////////////////////////
+/////////////////// Draw Functions ////////////////////////
+///////////////////////////////////////////////////////////
+
+function drawRotatedImg(img, x, y, angle, context)
+{
+   context.save();
+   context.translate(x+img.width/2,y+img.height/2);
+   context.rotate(angle*Math.PI/180);
+   context.translate(-x-img.width/2,-y-img.height/2);
+   context.drawImage(img,x,y);
+   context.restore();
 }
